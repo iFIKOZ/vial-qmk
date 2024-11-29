e61f014bcbb9229266addf9d7df4a7799c26e23e
iFIKOZ <fikocool@gmail.com>
2024-11-29 15:39:02 +0700

    first commit

---
+++ keyboards/fikoz/same60/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x96, 0xA1, 0xDA, 0x4B, 0x0E, 0x94, 0x6A, 0x7F}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 4}
+#define VIAL_UNLOCK_COMBO_COLS {0, 0}
---
+++ keyboards/fikoz/same60/rules.mk
@@ -0,0 +1,3 @@
+# This file intentionally left blank
+# Bootloader selection
+BOOTLOADER = stm32-dfu
